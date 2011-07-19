4acd3aef3cf9c0569a5cadc20b8e2a6dea5e1154
Stephen Walker <stephenwalker1988@gmail.com>
2011-07-19 16:23:32 -0500

    Updated ant file

--- tests/MockIOTest.java
+++ tests/MockIOTest.java
@@ -1,6 +1,8 @@
 import junit.framework.TestCase;
 
 public class MockIOTest extends TestCase {
+  IO IOStream = new MockIO();
+
   public void testOne() throws Exception {
     assertEquals(true, true);
   }
